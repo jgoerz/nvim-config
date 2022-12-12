e356405132e133f98985cf417cd69b4099044eba
Jesse Goerz <jesse@goerz.us>
2022-12-11 20:34:22 -0500

    Add in completion and folding

--- init.lua
+++ init.lua
@@ -30,6 +30,9 @@ local options = {
 }
 vim.opt.shortmess:append "c"
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
