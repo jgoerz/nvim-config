4f75a55890ed7d9d306fb468f31ec5e065c53a51
Jesse Goerz <jesse@goerz.us>
2023-04-22 10:02:26 -0400

    Remove old configuration

--- lua/user/lsp-java.lua
+++
@@ -1,6 +0,0 @@
-
-require'lspconfig'.jdtls.setup{
-  --[[ on_attach=require'completion'.on_attach, ]]
-  cmd = {"~/.local/share/nvim/lsp_servers/jdtls/bin/jdtls.py"},
-  root_dir = require'lspconfig/util'.root_pattern(".git", "pom.xml"),
-}
